db2ed4b40b0fd73632229364a87147c29715bed1
Michal Kolbusz <michalkolbusz@gmail.com>
2017-12-07 23:52:45 +0100

    move chalk dependency

--- src/main.js
+++ src/main.js
@@ -3,12 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// import Buefy from 'buefy'
-// import 'buefy/lib/buefy.css'
 
 Vue.config.productionTip = false
 
-// Vue.use(Buefy);
 
 /* eslint-disable no-new */
 new Vue({

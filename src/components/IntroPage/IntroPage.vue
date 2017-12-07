d56945caa0a7f88db5a16c7d23361fdd9e5897a5
Michal Kolbusz <michalkolbusz@gmail.com>
2017-12-07 23:40:44 +0100

    Init commit

---
+++ src/components/IntroPage/IntroPage.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="background">
+
+  <!-- <img src="../../assets/images/hexa.svg" alt="Kiwi standing on oval" class="hexa" :class="getClassName(n)" :key="n" v-for="n in hexas"> -->
+  <div class="cake">
+    <img src="../../assets/images/cake.png" alt="">
+    <img src="../../assets/images/cake-hover.png" alt="">
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'IntroPage',
+  data () {
+    return {
+      hexas: [1, 2, 3, 4, 5, 6]
+    }
+  },
+  methods: {
+    getClassName: function (n) {
+      return 'hexa-' + n
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+  @import 'intropage.scss';
+</style>
